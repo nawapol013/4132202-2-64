c706377b3950a52015558dc83e284ed871f3a765
nawapol013 <630112418013@bru.ac.th>
2021-12-27 12:00:45 +0700

    LAB7

---
+++ lab3/src/components/Dom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const age = 16;
+
+const dom = (
+  <>
+   <h1> Hello Rect!! </h1>
+   <p> My name is Nava ,{age} year old. </p>
+  </>
+);
+
+function Dom(){
+    return dom;
+}
+
+export default Dom
--- lab3/src/index.js
+++ lab3/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import KAK from './components/Dom'
+import Car from "./components/Car";
+import Table from './components/Table';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+   <KAK />
+   <Car />
+   <Table/>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
